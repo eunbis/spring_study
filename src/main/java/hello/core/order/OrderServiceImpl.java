2d7bbcf262ea6177ef4ec8d7201f3db24eb477b9
eunbi <pisr25@naver.com>
2022-03-15 13:45:11 +0900

    Revert "@Configuration과 싱글톤"
    This reverts commit 15c2efa67ddcfa21efa2f671e8b42e8d5ad59544.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -40,9 +40,4 @@ public class OrderServiceImpl implements OrderService{
         //주문 정보 반환
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
-
-    //테스트 용도
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
 }
